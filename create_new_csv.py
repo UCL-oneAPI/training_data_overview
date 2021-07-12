a4abe1f93f973cd04ee919996444a26061644c5f
costa-e <ehrensberger.c@me.com>
2021-07-12 12:24:04 +0200

    fix csv path

--- create_new_csv.py
+++ create_new_csv.py
@@ -2,8 +2,7 @@ import csv
 
 
 def create_csv():
-    # to reset sample data, use path 'warnings_data.csv' instead
-    with open('../../../training_data_overview/warnings_data.csv', 'w', newline='') as csvfile:
+    with open('warnings_data.csv', 'w', newline='') as csvfile:
         fieldnames = ['warning_code', 'project_name', 'file_path', 'line_in_file']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
